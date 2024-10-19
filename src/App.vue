10b0b3b28f30404979916c0e0c87f32765fadcd3
z-noname <cyber.cnoname@gmail.com>
2024-10-19 22:22:09 +0700

    set-default-home

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,26 @@
   <div id="app">
     <NavBar />
     <div class="content">
-      <router-view />
+      <home></home>
     </div>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
+    Home,
     NavBar
   },
   mounted() {
     // You can also trigger navigation to Home on component mount
     this.$router.push('/');
-  }
+  },
+
 }
 </script>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,8 +10,8 @@
       </div>
       <ul class="navbar-menu">
         <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
+        <!--<li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>-->
       </ul>
     </div>
   </nav>
