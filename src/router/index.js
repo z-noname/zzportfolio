1f661e23bf71e2f3e3303bd8d54f025abd9edd1d
z-noname <cyber.cnoname@gmail.com>
2024-10-19 21:13:29 +0700

    update-route

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Contact from '../components/Contact.vue';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/zzportfolio', component: Home },
     { path: '/about', component: About },
     { path: '/contact', component: Contact },
 ];
